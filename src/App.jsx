ad7edb819b028d651213e1a2bbf0eaf3b6fb6750
DeerK <derek.rodriguez.mendoza@est.una.ac.cr>
2025-04-03 01:10:40 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,35 @@
-import { useState, useRef  } from 'react'
- import './App.css'
+
+
+import './App.css'
  import Home from './Components/Home'
+import Login from './Components/Login'
+import {useContext} from 'react'
+
+import { AuthContext } from './Context/AuthContext'
  
  function App() {
  
-     const emailRef = useRef()
-     const passwordRef = useRef('')
-     const [error, setError] = useState('')
-     const [ user, setUser] = useState(null)
-     
+    
+ const { user } = useContext(AuthContext)
  
- const login = (email, password) => {
-   // Fake login check
-   if (email === "admin" && password === "1234") {    
-     
-     return true;
-   }
-   return false;
- };
- 
- 
- const handleLogin = () => {
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
-    const isSuccess = login(email, password);
-    if(!isSuccess)   
-    {
-       setError("Credenciales incorrectas");
-    }
-    else{
-    setUser({email})
-    } 
- }
  
    return (
      <>
  
  <h2>Login</h2>
-         
-         {
-           !user ?      
-           (<div>
-             <input 
-             type="email"
-             placeholder='Correo'
-             ref={emailRef}
-             />
-             <br/>
-             <input
-             type="password"
-             placeholder='Contraseña'
-             ref={passwordRef}
-             />
-             <br/>
-             <button onClick={handleLogin}>Ingresar</button>
-             { error && <p style={{ color: "red" }}>{error}</p> }
-           </div>) : 
-           (
-             <Home email={user.email} />
-           )
-       }
+
+        
+          <div>
+          {
+            !user ?    
+              <Login/>
+              : 
+            
+              <Home/>
+            
+        }
+        </div>
+       
           
  
                  
---
+++ src/Components/Login.jsx
@@ -0,0 +1,45 @@
+
+import { useContext } from "react"
+import { AuthContext } from '../Context/AuthContext'
+import { useUserState } from '../Hooks/useUserState'
+
+export default function Login () {
+
+    const {setUser} = useContext(AuthContext)
+    const { emailRef, passwordRef, error, login, setError} = useUserState()
+
+    
+      
+      const handleLogin = () => {
+         const email = emailRef.current.value;
+         const password = passwordRef.current.value;
+         const isSuccess = login(email, password);
+         if(!isSuccess)   
+         {
+            setError("Credenciales incorrectas");
+         }
+         else{
+            console.log("Set user State")
+         setUser({email})
+         } 
+      }
+    
+    return (
+        <div>
+             <input 
+             type="email"
+             placeholder='Correo'
+             ref={emailRef}
+             />
+             <br/>
+             <input
+             type="password"
+             placeholder='Contraseña'
+             ref={passwordRef}
+             />
+             <br/>
+             <button onClick={handleLogin}>Ingresar</button>
+             { error && <p style={{ color: "red" }}>{error}</p> }
+           </div>
+    )
+}
